123f52efc55a8ef90e962d016e08d9569332ce32
nrjgit <mail.nrj@gmail.com>
2025-03-13 15:28:05 +0530

    mongoose lib and feature added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const mongoose = require('mongoose'); 
+
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +10,13 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.Promise = Promise; 
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.error('MongoDB Connection Error:', err));
+
 
 // API Route Example
 app.get('/api/hello', (req, res) => {
